41f2e7537f0af7259a6129899e79a5ddc983b186
Lance Chatwell <lancechat@yahoo.com>
2023-10-24 21:17:28 +0200

    Update main.go
    refractored the code a bit

--- guestbook-go/app/main.go
+++ guestbook-go/app/main.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -15,71 +17,145 @@ import (
 	"github.com/urfave/negroni"
 )
 
-var (
-	rdb *redis.Client
+var rdb *redis.Client
+
+const (
+	redisAddr     = "localhost:6379"
+	redisPassword = ""
+	redisDB       = 0
+	serverAddr    = ":8080"
 )
 
+func main() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPassword,
+		DB:       redisDB,
+	})
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+		// You can choose to exit the application gracefully or take other recovery actions here.
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", ShowIndex)
+	router.HandleFunc("/lrange/{key}", ListRangeHandler).Methods("GET")
+	router.HandleFunc("/lrange", ListAllHandler).Methods("GET")
+	router.HandleFunc("/rpush/{key}/{value}", ListPushHandler).Methods("POST")
+	router.HandleFunc("/info", InfoHandler).Methods("GET")
+	router.HandleFunc("/env", EnvHandler)
+
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	srv := &http.Server{
+		Addr:         serverAddr,
+		Handler:      n,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	gracefulShutdown(srv)
+}
+func HandleError(w http.ResponseWriter, err error, context string, statusCode int) bool {
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error in context '%s': %v", context, err)
+		log.Printf("[%s] %s\n", time.Now().Format(time.RFC3339), errorMsg)
+		http.Error(w, errorMsg, statusCode)
+		return true
+	}
+	return false
+}
+
+func gracefulShutdown(srv *http.Server) {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	<-sigCh
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("Server shutdown error: %v\n", err)
+	}
+
+	log.Println("Server stopped gracefully.")
+}
+
+// Other handlers and utility functions...
 func ShowIndex(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Set("Content-Type", "application/text")
-	//w.Header().Set("X-Content-Type-Options", "nosniff")
 	http.ServeFile(w, r, "public/index.html")
 }
 
 func ListRangeHandler(w http.ResponseWriter, r *http.Request) {
 	key := mux.Vars(r)["key"]
-	list := rdb.LRange(context.Background(), key, 0, -1)
-
-	var members []string
-	err := list.ScanSlice(&members)
+	list, err := rdb.LRange(context.Background(), key, 0, -1).Result()
 	if err != nil {
-		HandleError(nil, err)
+		handleError(w, err)
+		return
 	}
 
-	membersJSON, err := json.MarshalIndent(members, "", "  ")
-	if err != nil {
-		HandleError(nil, err)
-	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
-	w.Write(membersJSON)
+	if err := json.NewEncoder(w).Encode(list); err != nil {
+		handleError(w, err)
+	}
+}
+
+func handleError(w http.ResponseWriter, err error) {
+	panic("unimplemented")
 }
 func ListAllHandler(w http.ResponseWriter, r *http.Request) {
 	iter := rdb.Scan(context.Background(), 0, "prefix:*", 0).Iterator()
 	var keys []string
-	for iter.Next(context.Background()) {
+	for {
+		if !iter.Next(context.Background()) {
+			break
+		}
 		keys = append(keys, iter.Val())
 	}
+
 	if err := iter.Err(); err != nil {
-		HandleError(w, err)
-	} else {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(strings.Join(keys, "\n")))
+		handleError(w, err)
+		return
 	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(strings.Join(keys, "\n")))
 }
 
 func ListPushHandler(w http.ResponseWriter, r *http.Request) {
 	key := mux.Vars(r)["key"]
 	value := mux.Vars(r)["value"]
-	list := rdb.RPush(context.Background(), key, value)
 
-	err := list.Err()
-	if err != nil {
-		HandleError(w, err)
-	} else {
-		w.WriteHeader(http.StatusOK)
-		ListRangeHandler(w, r)
+	if err := rdb.RPush(context.Background(), key, value).Err(); err != nil {
+		handleError(w, err)
+		return
 	}
-}
 
+	w.WriteHeader(http.StatusOK)
+	ListRangeHandler(w, r)
+}
 func InfoHandler(w http.ResponseWriter, r *http.Request) {
 	info, err := rdb.Info(context.Background()).Result()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		handleError(w, err)
 		return
 	}
-	//w.Header().Set("Content-Type", "application/html")
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(info))
 }
-
 func EnvHandler(w http.ResponseWriter, r *http.Request) {
 	environment := make(map[string]string)
 
@@ -90,62 +166,10 @@ func EnvHandler(w http.ResponseWriter, r *http.Request) {
 		environment[key] = val
 	}
 
-	envJSON, err := json.MarshalIndent(environment, "", "  ")
-	if err != nil {
-		HandleError(nil, err)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(environment); err != nil {
+		handleError(w, err)
 	}
-
-	w.Write(envJSON)
-}
-
-func HandleError(result interface{}, err error) (r interface{}) {
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	return result
-}
-
-func main() {
-	ctx := context.Background()
-	rdb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	iter := rdb.Scan(ctx, 0, "prefix:*", 0).Iterator()
-	for iter.Next(ctx) {
-		fmt.Println("keys", iter.Val())
-	}
-	if err := iter.Err(); err != nil {
-		log.Println(err)
-	}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/", ShowIndex)
-	router.HandleFunc("/lrange/{key}", func(w http.ResponseWriter, r *http.Request) {
-		ListRangeHandler(w, r)
-	}).Methods("GET")
-	router.HandleFunc("/lrange", ListAllHandler).Methods("GET")
-	router.HandleFunc("/rpush/{key}/{value}", func(w http.ResponseWriter, r *http.Request) {
-		ListPushHandler(w, r)
-	}).Methods("POST")
-	router.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
-		InfoHandler(w, r)
-	}).Methods("GET")
-	router.HandleFunc("/env", EnvHandler)
-
-	n := negroni.Classic()
-	n.Use(negroni.NewLogger())
-	n.UseHandler(router)
-
-	srv := &http.Server{
-		Addr:         ":8080",
-		Handler:      n,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	log.Fatal(srv.ListenAndServe())
 }
